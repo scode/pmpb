0e192a0592674d125c59c682c35f757ec8a84821
Peter Schuller <peter.schuller@infidyne.com>
2008-09-17 20:38:47 -0159

    * install portconf before anything else so that ports.conf changes   that affect zsh or portupgrade and their deps, have effect

--- prebuild.sh
+++ prebuild.sh
@@ -2,12 +2,12 @@
 
 set -e
 
+cd /usr/ports/ports-mgmt/portconf && make BATCH=1 clean package-recursive
+cd /usr/ports/ports-mgmt/portconf && make clean
 cd /usr/ports/shells/zsh && make BATCH=1 clean package-recursive
 cd /usr/ports/shells/zsh && make clean
 cd /usr/ports/ports-mgmt/portupgrade && make BATCH=1 clean package-recursive
 cd /usr/ports/ports-mgmg/portupgrade && make clean
-cd /usr/ports/ports-mgmt/portconf && make BATCH=1 clean package-recursive
-cd /usr/ports/ports-mgmt/portconf && make clean
 
 
 
