e5d4c2d4c6564e4f23f2ef8bcaff604abba883ae
Peter Schuller <peter.schuller@infidyne.com>
2008-09-18 16:00:26 -0159

    * fix typo preventing cleaning of portupgrade

--- prebuild.sh
+++ prebuild.sh
@@ -7,7 +7,7 @@ cd /usr/ports/ports-mgmt/portconf && make clean
 cd /usr/ports/shells/zsh && make BATCH=1 clean package-recursive
 cd /usr/ports/shells/zsh && make clean
 cd /usr/ports/ports-mgmt/portupgrade && make BATCH=1 clean package-recursive
-cd /usr/ports/ports-mgmg/portupgrade && make clean
+cd /usr/ports/ports-mgmt/portupgrade && make clean
 
 
 
