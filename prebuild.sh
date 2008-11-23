537d87421a13058a248546eaabe4c55a92cf5b10
Peter Schuller <peter.schuller@infidyne.com>
2008-11-23 09:21:08 -0159

    * install screen as part of prebuild process

--- prebuild.sh
+++ prebuild.sh
@@ -9,5 +9,9 @@ cd /usr/ports/shells/zsh && make clean
 cd /usr/ports/ports-mgmt/portupgrade && make BATCH=1 clean package-recursive
 cd /usr/ports/ports-mgmt/portupgrade && make clean
 
+# not needed by the scripts, however it is useful to kick off the full build
+# in a screen and thus anoying when screen is missing
+cd /usr/ports/sysutils/screen && make BATCH=1 clean package-recursive
+cd /usr/ports/sysutils/screen && make clean
 
 
