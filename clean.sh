ac151432783fa80a6d1df9e871f044568c3559b6
Peter Schuller <peter.schuller@infidyne.com>
2010-05-06 14:33:38 +0200

    On clean, set BATCH=1 and use -f.

--- clean.sh
+++ clean.sh
@@ -6,7 +6,7 @@ set -e
 # twist over *something* in between upgrades. Totally nuking the
 # database tends to translate into successful operation.
 
-pkg_delete -a
+BATCH=1 pkg_delete -a -a
 rm -rf /usr/ports/packages/All
 rm /var/db/pkg/pkgdb.db
 
